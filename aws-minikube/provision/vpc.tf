eed68f609993af489842cbbfb9660a458146d7a2
irvnet <irvnet@gmail.com>
2024-02-25 20:04:38 -0500

    simplify subnets

--- aws-minikube/provision/vpc.tf
+++ aws-minikube/provision/vpc.tf
@@ -2,13 +2,13 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "base-network"
+  name = "cka-minikube-vpc"
 
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
 
   enable_ipv6             = false
   enable_nat_gateway      = false
@@ -17,15 +17,19 @@ module "vpc" {
   map_public_ip_on_launch = true
 
   public_subnet_tags = {
-    Name = "Public Subnet"
+    Name = "minikube public subnet"
+  }
+
+  private_subnet_tags = {
+    Name = "minikube private subnet"
   }
 
   tags = {
     Owner       = "irvnet"
-    Environment = "cka-test"
+    Environment = "cka-minikube-test"
   }
 
   vpc_tags = {
-    Name = "k8s dev Environment"
+    Name = "CKA minikube test env"
   }
 }
