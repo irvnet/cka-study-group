24489a6b42de7aa040eca8d7104470c0cf7ebcb9
irvnet <irvnet@gmail.com>
2024-02-25 20:07:07 -0500

    add: simplified packer prep script

--- aws-kubeadm/ami/minimal-prep.sh
+++ aws-kubeadm/ami/minimal-prep.sh
@@ -33,3 +33,12 @@ EOF
 
 ## Apply sysctl params without reboot
 sudo sysctl --system
+
+## setup repos & install containerd
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
