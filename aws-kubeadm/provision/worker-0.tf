0807744ff9a273f9a54288fc2ea86b468c4627c5
irvnet <irvnet@gmail.com>
2024-03-01 16:07:41 -0500

    add: provision scripts for worker nodes

---
+++ aws-kubeadm/provision/worker-0.tf
@@ -0,0 +1,21 @@
+
+resource "aws_instance" "worker-0" {
+  ami           = var.AMI_ID
+  instance_type = "t2.large"
+
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = "20"
+    delete_on_termination = "true"
+  }
+
+  subnet_id              = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [aws_security_group.all-traffic.id]
+  key_name               = "asamples"
+
+  tags = {
+    Name = "kubeadm-worker-0"
+    proj = "cka-review"
+  }
+
+}
