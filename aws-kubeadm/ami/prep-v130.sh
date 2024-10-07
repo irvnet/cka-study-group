1aa5dec1268fbfdddaf54bd43c036422e96cb560
irvnet <irvnet@gmail.com>
2024-10-07 13:12:32 -0400

    update kubeadm node tempalate to k8s v1.3 release

---
+++ aws-kubeadm/ami/prep-v130.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+echo 'prepping environment:  kubeadm v1.30 / ubuntu 22.04 LTS... '
+
+## config for unattended install
+sudo sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
+echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
+
+## install prep
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get -y install apt-transport-https ca-certificates curl gpg socat
+
+## turn off swap
+swapoff -a
+
+## load required modules
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+## configure to load on boot
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+## config sysctl updates to persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+## Apply sysctl params without reboot
+sudo sysctl --system
+
+## setup docker repos
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+## install containerd
+sudo apt-get update && sudo apt-get install containerd.io -y
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+sudo sed -e 's/SystemdCgroup = false/SystemdCgroup = true/g' -i /etc/containerd/config.toml
+sudo systemctl restart containerd
